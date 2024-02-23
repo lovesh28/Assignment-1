70bb9c8c0f2d45706b1703a59810d982b04952cf
lovesh28 <loveshchaudhari28@gmail.com>
2024-02-23 19:38:40 +0530

    responsive-desined

--- src/component/Product/ViewProduct.jsx
+++ src/component/Product/ViewProduct.jsx
@@ -1,45 +1,72 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import "./Product.css";
 
 const ViewProduct = () => {
+  const { id } = useParams();
+  const [product, setProduct] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [Error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchdata = async () => {
+      try {
+        const response = await axios.get(
+          `https://dummyjson.com/products/${id}`
+        );
+        setProduct(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.log("Error Fetching Data", error);
+        setError(error);
+        setLoading(false);
+      }
+    };
+    fetchdata();
+  }, [id]);
+
+  console.log("Loading:", loading);
+  console.log("Error", error);
+  console.log("Product:", product);
+
   return (
-  <>
-  <h1 className="product-title">Product Details</h1>
-  <div className="product-details d-flex justify-content-center align-items-center">
-    { loading ? (
-        <p>loading details...</p>
-    ):(
-        <div className="product-detail-card">
+    <>
+      <h1 className="product-title">Product Details</h1>
+      <div className="product-details d-flex justify-content-center align-items-center">
+        {loading ? (
+          <p>loading details...</p>
+        ) : error ? (
+          <p>Error: {error.message}</p>
+        ) : (
+          <div className="product-detail-card">
             <div className="product-img-container">
-                <img src={product.thumbnail} 
+              <img
+                src={product.thumbnail}
                 alt={product.title}
-                style={{width:"300px"}} />
+                style={{ width: "300px" }}
+              />
             </div>
             <div className="product-info">
-                <h3>
-                    {product.title}
-                </h3>
-                <p className="product-text">{product.brand} </p>
-                <p>{product.description}</p>
-                
-
+              <h3>{product.title}</h3>
+              <p className="product-text">{product.brand} </p>
+              <p>{product.description}</p>
+              <p className="product-text">{product.price} </p>
+              Discount:{""}
+              <p className="product-text">{product.discountPercentage}%</p>
+              <p className="product-text fs-6 fw-bold">
+                {" "}
+                {product.stock} left in stock
+              </p>
+              <p>
+                <strong>Rating:</strong>{" "}
+                <span className="star">{product.rating} ☆</span>
+              </p>
             </div>
-        </div>
-    ) }
-
-
-
-
-
-
-
-
-
-
-
-
-  </div>
-
-  </>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
